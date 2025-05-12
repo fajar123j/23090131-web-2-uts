34ccf3b1b5282879ff6334e073fa9a1493cd0ca1
fajar <fr7946971@gmail.com>
2025-05-12 16:28:17 +0700

    feat: crud produk

---
+++ app/Models/Products.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Products extends Model
+{
+    use hasFactory;
+    protected $table = 'products';
+
+    public function category()
+    {
+        return $this->belongsTo(Categories::class, 'product_category_id');
+    }
+}
